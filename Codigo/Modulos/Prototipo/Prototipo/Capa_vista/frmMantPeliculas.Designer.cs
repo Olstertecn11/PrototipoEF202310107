0f5e85efefd33ddfaf349b0cd6f8f22a78a82a76
Oliver Tzunun <otzunund@miumg.edu.gt>
2023-11-21 09:14:20 -0600

    movies form working like crud

--- Codigo/Modulos/Prototipo/Prototipo/Capa_vista/frmMantPeliculas.Designer.cs
+++ Codigo/Modulos/Prototipo/Prototipo/Capa_vista/frmMantPeliculas.Designer.cs
@@ -29,12 +29,207 @@ namespace Vista_PrototipoMenu
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGrid = new NavegadorVista.Navegador();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtClasificacion = new System.Windows.Forms.TextBox();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtIdioma = new System.Windows.Forms.TextBox();
+            this.txtSubtitulado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.Location = new System.Drawing.Point(98, 12);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(574, 152);
+            this.dataGrid.TabIndex = 0;
+            this.dataGrid.Load += new System.EventHandler(this.navegador1_Load);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(98, 316);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(574, 167);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Tag = "peliculas";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(98, 194);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 2;
+            this.txtId.Tag = "idPeliculas";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(227, 194);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 3;
+            this.txtNombre.Tag = "Nombre";
+            // 
+            // txtClasificacion
+            // 
+            this.txtClasificacion.Location = new System.Drawing.Point(352, 194);
+            this.txtClasificacion.Name = "txtClasificacion";
+            this.txtClasificacion.Size = new System.Drawing.Size(100, 20);
+            this.txtClasificacion.TabIndex = 4;
+            this.txtClasificacion.Tag = "Clasificacion";
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.Location = new System.Drawing.Point(475, 194);
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(100, 20);
+            this.txtGenero.TabIndex = 5;
+            this.txtGenero.Tag = "Genero";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(352, 257);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 8;
+            this.txtPrecio.Tag = "precio";
+            // 
+            // txtIdioma
+            // 
+            this.txtIdioma.Location = new System.Drawing.Point(227, 257);
+            this.txtIdioma.Name = "txtIdioma";
+            this.txtIdioma.Size = new System.Drawing.Size(100, 20);
+            this.txtIdioma.TabIndex = 7;
+            this.txtIdioma.Tag = "Idioma";
+            // 
+            // txtSubtitulado
+            // 
+            this.txtSubtitulado.Location = new System.Drawing.Point(98, 257);
+            this.txtSubtitulado.Name = "txtSubtitulado";
+            this.txtSubtitulado.Size = new System.Drawing.Size(100, 20);
+            this.txtSubtitulado.TabIndex = 6;
+            this.txtSubtitulado.Tag = "Subtitulado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Clasificacion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(472, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Genero";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(98, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Subtitulado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(224, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Idioma";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(349, 241);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Precio";
+            // 
+            // frmMantPeliculas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(823, 510);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtIdioma);
+            this.Controls.Add(this.txtSubtitulado);
+            this.Controls.Add(this.txtGenero);
+            this.Controls.Add(this.txtClasificacion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGrid);
+            this.Name = "frmMantPeliculas";
             this.Text = "frmMantPeliculas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private NavegadorVista.Navegador dataGrid;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtClasificacion;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtIdioma;
+        private System.Windows.Forms.TextBox txtSubtitulado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
--- Codigo/Modulos/Prototipo/Prototipo/Capa_vista/frmMantPeliculas.cs
+++ Codigo/Modulos/Prototipo/Prototipo/Capa_vista/frmMantPeliculas.cs
@@ -12,9 +12,23 @@ namespace Vista_PrototipoMenu
 {
     public partial class frmMantPeliculas : Form
     {
+        Seguridad_Controlador.Controlador cn = new Seguridad_Controlador.Controlador();
         public frmMantPeliculas()
         {
             InitializeComponent();
+        }
+
+        private void navegador1_Load(object sender, EventArgs e)
+        {
+            NavegadorVista.Navegador.idApp = "7001";
+            dataGrid.actual = this;
+            dataGrid.tabla = this.dataGridView1;
+            TextBox[] Grupotextbox = { txtId, txtNombre, txtClasificacion, txtGenero, txtSubtitulado, txtIdioma, txtPrecio };
+            TextBox[] Idtextbox = { txtId, txtNombre };
+            dataGrid.textbox = Grupotextbox;
+            dataGrid.textboxi = Idtextbox;
+            dataGrid.cargar(dataGridView1, Grupotextbox, cn.getNombreBd());
+
         }
     }
 }
