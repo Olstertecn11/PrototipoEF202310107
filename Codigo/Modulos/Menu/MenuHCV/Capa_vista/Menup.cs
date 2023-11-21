6ea8678d889c686c7054d40f7f37f2fed4e8a8b7
Oliver Tzunun <otzunund@miumg.edu.gt>
2023-11-21 08:58:02 -0600

    splah and sectionn menu working

---
+++ Codigo/Modulos/Menu/MenuHCV/Capa_vista/Menup.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Seguridad_Controlador;
+using Vista_Seguridad;
+using Vista_PrototipoMenu;
+
+namespace MenuHCV
+{
+    public partial class Menup : Form
+    {
+        Controlador cn = new Controlador();
+
+        public Menup()
+        {
+            InitializeComponent();
+
+            Button[] apps = { btnseguridad, btncompras, btnVentas, btnInventario, btnBancos, btnNominas };
+            cn.deshabilitarApps(apps);
+
+            cn.getAccesoApp(1000, apps[0]);  //Modulo Seguridad
+            cn.getAccesoApp(2000, apps[1]);  //Modulo Compras y Cxp
+            cn.getAccesoApp(3000, apps[2]);  //Modulo Ventas y Cxc
+            cn.getAccesoApp(7000, apps[3]);  //Modulo Inventarios
+            cn.getAccesoApp(5000, apps[4]);  //Modulo Bancos
+            cn.getAccesoApp(6000, apps[5]);  //Modulo Nominas
+
+
+
+        }
+        //Método que ingresa un valor a la bitacora de acuerdo el módulo
+        public void btnlogout_Click(object sender, EventArgs e)
+        {
+            MenuSeguridad b = new MenuSeguridad();
+            b.Show();
+            cn.setBtitacora("1000", "Ingreso Seguridad");
+            //this.Close();
+        }
+
+        public void btnVentas_Click(object sender, EventArgs e)
+        {
+            cn.setBtitacora("2000", "Ingreso Informes");
+        }
+        private void btnLogout_Click_1(object sender, EventArgs e)
+        {
+            Login b = new Login();
+            cn.setBtitacora("999", "Cerro Sesion");
+            b.Show();
+            this.Close();
+        }
+        private void btncompras_Click(object sender, EventArgs e)
+        {
+            cn.setBtitacora("3000", "Ingreso Procesos");
+        }
+
+        private void btnInventario_Click(object sender, EventArgs e)
+        {
+            cn.setBtitacora("4000", "Ingreso Catalogos");
+            Vista_PrototipoMenu.MenuPrototipo frm = new Vista_PrototipoMenu.MenuPrototipo();
+            frm.Show();
+        }
+
+        private void btnBancos_Click(object sender, EventArgs e)
+        {
+            cn.setBtitacora("5000", "Ingreso Herramientas");
+        }
+
+        private void btnNominas_Click_1(object sender, EventArgs e)
+        {
+            cn.setBtitacora("6000", "Ingreso Ayuda");
+        }
+
+        private void btnHoteleria_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCxC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCxP_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPrototipo_Click(object sender, EventArgs e)
+        {
+            cn.setBtitacora("8000", "Ingreso Prototipo");
+        }
+
+    }
+}
---
+++ Codigo/Modulos/Menu/MenuHCV/Capa_vista/Menup.designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace MenuHCV
+{
+    partial class Menup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menup));
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.btnNominas = new System.Windows.Forms.Button();
+            this.btnBancos = new System.Windows.Forms.Button();
+            this.btnInventario = new System.Windows.Forms.Button();
+            this.btnCxP = new System.Windows.Forms.Button();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.btncompras = new System.Windows.Forms.Button();
+            this.btnCxC = new System.Windows.Forms.Button();
+            this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnseguridad = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelLogo.SuspendLayout();
+            this.panelSideMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.btnNominas);
+            this.panelLogo.Controls.Add(this.btnBancos);
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Controls.Add(this.btnInventario);
+            this.panelLogo.Controls.Add(this.btnCxP);
+            this.panelLogo.Controls.Add(this.btnVentas);
+            this.panelLogo.Controls.Add(this.btncompras);
+            this.panelLogo.Controls.Add(this.btnCxC);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(238, 483);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // btnNominas
+            // 
+            this.btnNominas.BackColor = System.Drawing.Color.DarkGray;
+            this.btnNominas.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnNominas.FlatAppearance.BorderSize = 0;
+            this.btnNominas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNominas.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNominas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnNominas.Location = new System.Drawing.Point(-1, 277);
+            this.btnNominas.Name = "btnNominas";
+            this.btnNominas.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnNominas.Size = new System.Drawing.Size(236, 31);
+            this.btnNominas.TabIndex = 17;
+            this.btnNominas.Text = "Ayuda";
+            this.btnNominas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNominas.UseVisualStyleBackColor = false;
+            this.btnNominas.Click += new System.EventHandler(this.btnNominas_Click_1);
+            // 
+            // btnBancos
+            // 
+            this.btnBancos.BackColor = System.Drawing.Color.DarkGray;
+            this.btnBancos.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnBancos.FlatAppearance.BorderSize = 0;
+            this.btnBancos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBancos.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBancos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnBancos.Location = new System.Drawing.Point(-1, 248);
+            this.btnBancos.Name = "btnBancos";
+            this.btnBancos.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnBancos.Size = new System.Drawing.Size(239, 31);
+            this.btnBancos.TabIndex = 16;
+            this.btnBancos.Text = "Herramientas";
+            this.btnBancos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBancos.UseVisualStyleBackColor = false;
+            this.btnBancos.Click += new System.EventHandler(this.btnBancos_Click);
+            // 
+            // btnInventario
+            // 
+            this.btnInventario.BackColor = System.Drawing.Color.DarkGray;
+            this.btnInventario.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnInventario.FlatAppearance.BorderSize = 0;
+            this.btnInventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInventario.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnInventario.Location = new System.Drawing.Point(-1, 165);
+            this.btnInventario.Name = "btnInventario";
+            this.btnInventario.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnInventario.Size = new System.Drawing.Size(236, 27);
+            this.btnInventario.TabIndex = 15;
+            this.btnInventario.Text = "Catalogos";
+            this.btnInventario.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInventario.UseVisualStyleBackColor = false;
+            this.btnInventario.Click += new System.EventHandler(this.btnInventario_Click);
+            // 
+            // btnCxP
+            // 
+            this.btnCxP.BackColor = System.Drawing.Color.SlateGray;
+            this.btnCxP.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnCxP.Enabled = false;
+            this.btnCxP.FlatAppearance.BorderSize = 0;
+            this.btnCxP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCxP.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCxP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnCxP.Location = new System.Drawing.Point(-1, 439);
+            this.btnCxP.Name = "btnCxP";
+            this.btnCxP.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnCxP.Size = new System.Drawing.Size(312, 24);
+            this.btnCxP.TabIndex = 8;
+            this.btnCxP.Text = "CxP";
+            this.btnCxP.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCxP.UseVisualStyleBackColor = false;
+            this.btnCxP.Visible = false;
+            this.btnCxP.Click += new System.EventHandler(this.btnCxP_Click);
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.BackColor = System.Drawing.Color.DarkGray;
+            this.btnVentas.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnVentas.FlatAppearance.BorderSize = 0;
+            this.btnVentas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVentas.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVentas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnVentas.Location = new System.Drawing.Point(0, 219);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnVentas.Size = new System.Drawing.Size(236, 31);
+            this.btnVentas.TabIndex = 0;
+            this.btnVentas.Text = "Informes";
+            this.btnVentas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVentas.UseVisualStyleBackColor = false;
+            this.btnVentas.Click += new System.EventHandler(this.btnVentas_Click);
+            // 
+            // btncompras
+            // 
+            this.btncompras.BackColor = System.Drawing.Color.DarkGray;
+            this.btncompras.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btncompras.FlatAppearance.BorderSize = 0;
+            this.btncompras.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncompras.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncompras.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btncompras.Location = new System.Drawing.Point(-1, 190);
+            this.btncompras.Name = "btncompras";
+            this.btncompras.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btncompras.Size = new System.Drawing.Size(239, 29);
+            this.btncompras.TabIndex = 2;
+            this.btncompras.Text = "Procesos";
+            this.btncompras.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btncompras.UseVisualStyleBackColor = false;
+            this.btncompras.Click += new System.EventHandler(this.btncompras_Click);
+            // 
+            // btnCxC
+            // 
+            this.btnCxC.BackColor = System.Drawing.Color.SlateGray;
+            this.btnCxC.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnCxC.Enabled = false;
+            this.btnCxC.FlatAppearance.BorderSize = 0;
+            this.btnCxC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCxC.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCxC.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnCxC.Location = new System.Drawing.Point(0, 461);
+            this.btnCxC.Name = "btnCxC";
+            this.btnCxC.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnCxC.Size = new System.Drawing.Size(390, 23);
+            this.btnCxC.TabIndex = 4;
+            this.btnCxC.Text = "CxC";
+            this.btnCxC.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCxC.UseVisualStyleBackColor = false;
+            this.btnCxC.Visible = false;
+            this.btnCxC.Click += new System.EventHandler(this.btnCxC_Click);
+            // 
+            // panelSideMenu
+            // 
+            this.panelSideMenu.AutoScroll = true;
+            this.panelSideMenu.BackColor = System.Drawing.Color.DarkGray;
+            this.panelSideMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panelSideMenu.Controls.Add(this.btnLogout);
+            this.panelSideMenu.Controls.Add(this.btnseguridad);
+            this.panelSideMenu.Controls.Add(this.panelLogo);
+            this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelSideMenu.Name = "panelSideMenu";
+            this.panelSideMenu.Size = new System.Drawing.Size(238, 592);
+            this.panelSideMenu.TabIndex = 46;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.DarkGray;
+            this.btnLogout.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnLogout.Location = new System.Drawing.Point(-1, 392);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnLogout.Size = new System.Drawing.Size(226, 41);
+            this.btnLogout.TabIndex = 14;
+            this.btnLogout.Text = "CERRAR SESIÓN";
+            this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click_1);
+            // 
+            // btnseguridad
+            // 
+            this.btnseguridad.BackColor = System.Drawing.Color.DarkGray;
+            this.btnseguridad.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnseguridad.FlatAppearance.BorderSize = 0;
+            this.btnseguridad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnseguridad.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnseguridad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnseguridad.Location = new System.Drawing.Point(0, 370);
+            this.btnseguridad.Name = "btnseguridad";
+            this.btnseguridad.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnseguridad.Size = new System.Drawing.Size(225, 22);
+            this.btnseguridad.TabIndex = 13;
+            this.btnseguridad.Text = "SEGURIDAD";
+            this.btnseguridad.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnseguridad.UseVisualStyleBackColor = false;
+            this.btnseguridad.Click += new System.EventHandler(this.btnlogout_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.pictureBox1.Image = global::Vista_MenuHCV.Properties.Resources.cinevision1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Menup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 592);
+            this.Controls.Add(this.panelSideMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Menup";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu General";
+            this.panelLogo.ResumeLayout(false);
+            this.panelSideMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelSideMenu;
+        public System.Windows.Forms.Button btnVentas;
+        public System.Windows.Forms.Button btncompras;
+        public System.Windows.Forms.Button btnCxC;
+        public System.Windows.Forms.Button btnCxP;
+        public System.Windows.Forms.Button btnseguridad;
+        public System.Windows.Forms.Button btnLogout;
+        public System.Windows.Forms.Button btnInventario;
+        public System.Windows.Forms.Button btnNominas;
+        public System.Windows.Forms.Button btnBancos;
+    }
+}
